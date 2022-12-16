7a92f271ddf704346ed4cb787aee88e55d07bb7f
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2022-12-16 15:58:55 +0100

    Prompt per la richiesta dati  all'utente

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Chiedo il nome all'utente 
+const fname = prompt("Come ti chiami?");
+console.log("L'utente si chiama: ", fname);
+
+// Chiedo il cognome all'utente 
+
+const lname = prompt("Qual è il tuo cognome?");
+console.log("Il cognome dell'utente è: ", lname);
+
+// Chiedo il colore preferito all'utente
+
+const favcol = prompt("Qual è il tuo colore preferito");
+console.log("Il colore preferito dell'utente è: ", favcol);
